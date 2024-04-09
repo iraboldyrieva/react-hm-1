2d5325b0fee0b7dd749c370068b619d6fa90ffb3
iraboldyrieva <iraboldyrieva@gmail.com>
2024-04-09 17:12:41 +0300

    add react-hw-1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import MyFunctionalComponent from "./MyFunctionalComponent";
+import MyClassComponent from "./MyClassComponent";
+
+import "./App.css";
 
 function App() {
+  const component = "functional";
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {component === "functional" ? (
+        <MyFunctionalComponent />
+      ) : (
+        <MyClassComponent />
+      )}
     </div>
   );
 }
